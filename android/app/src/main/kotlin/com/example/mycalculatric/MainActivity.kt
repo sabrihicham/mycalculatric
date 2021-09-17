c1eefe9b623f0caa6a6898d7f2b057380909251a
sabrihicham <sabri.hicham@univ-bechar.dz>
2021-09-17 16:26:29 +0100

    First Commit

---
+++ android/app/src/main/kotlin/com/example/mycalculatric/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mycalculatric
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+  class MyApp extends StatelessWidget {
+    const MyApp({Key? key}) : super(key: key);
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        body: Column(
+          children: [
+           Container(
+             color: Colors.brown,
+           ),
+           Container(
+             color: Colors.,
+           ),
+          ],
+        ),
+      );
+    }
+  }
